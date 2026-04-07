16357301bb6a5ed747bfa313cda31c280962f832
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-07 16:43:34 -0300

    first commit com estrutura inicial do projeto

---
+++ src/main/java/com/GabrielTiziano/NexWallet/NexWalletApplication.java
@@ -0,0 +1,13 @@
+package com.GabrielTiziano.NexWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexWalletApplication.class, args);
+	}
+
+}
