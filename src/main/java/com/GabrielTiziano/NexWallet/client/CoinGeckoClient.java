f0eb5e3e639b2289320b1fffd723f8110bbd335e
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-10 15:22:38 -0300

    feat: Cria o endpoint no client responsável por retornar as top 10 moedas mais valiosas na API do CoinGecko no momento em que a requisição é feita

--- src/main/java/com/GabrielTiziano/NexWallet/client/CoinGeckoClient.java
+++ src/main/java/com/GabrielTiziano/NexWallet/client/CoinGeckoClient.java
@@ -1,10 +1,12 @@
 package com.GabrielTiziano.NexWallet.client;
 
+import com.GabrielTiziano.NexWallet.dto.Top10CoinsDTO;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @FeignClient(name = "CoinGeckoClient", url = "${nexwallet.client.coingecko}")
@@ -14,5 +16,14 @@ public interface CoinGeckoClient {
     Map<String, Map<String, Double>> getPrice(
             @RequestParam("ids") String cryptoId,
             @RequestParam("vs_currencies") String currency
+    );
+
+    @Cacheable(cacheNames = "topCoins", key = "#currency + '-' + #perPage")
+    @GetMapping("/coins/markets")
+    List<Top10CoinsDTO> getTop10Coins(
+            @RequestParam("vs_currency") String currency,
+            @RequestParam("order") String order,
+            @RequestParam("per_page") Integer perPage,
+            @RequestParam("page") Integer page
     );
 }
