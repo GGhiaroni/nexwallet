c301cc31a5376ee3bada713850c9563532d536fe
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-10 18:22:54 -0300

    feat: Cria no UserRepository o método responsável por buscar o usuário pelo seu e-mail no banco de dados

--- src/main/java/com/GabrielTiziano/NexWallet/repository/UserRepository.java
+++ src/main/java/com/GabrielTiziano/NexWallet/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.GabrielTiziano.NexWallet.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 import com.GabrielTiziano.NexWallet.model.UserModel;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<UserModel, String> {
-
+    Optional<UserDetails> findUserByEmail(String email);
 }
