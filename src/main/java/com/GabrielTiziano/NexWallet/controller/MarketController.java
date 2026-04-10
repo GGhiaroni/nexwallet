a3d8f194b7994a70120ac0754a9b6656ba742ebe
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-10 15:24:05 -0300

    feat: Cria o novo endpoint responsável por retornar o top 10 moedas mais valiosas da API da CoinGecko no momento em que a requisição é realizada. Por padrão, retorna em USD, mas pode ser alterado para qualquer moeda através de parâmetro de url

--- src/main/java/com/GabrielTiziano/NexWallet/controller/MarketController.java
+++ src/main/java/com/GabrielTiziano/NexWallet/controller/MarketController.java
@@ -1,5 +1,6 @@
 package com.GabrielTiziano.NexWallet.controller;
 
+import com.GabrielTiziano.NexWallet.dto.Top10CoinsDTO;
 import com.GabrielTiziano.NexWallet.service.MarketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,18 +10,23 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/market/prices")
+@RequestMapping("/market")
 @RequiredArgsConstructor
 public class MarketController {
     private final MarketService marketService;
 
-    @GetMapping("/{cryptoId}")
+    @GetMapping("/prices/{cryptoId}")
     public ResponseEntity<Map<String, Map<String, Double>>> getSpecificPrice(@PathVariable String cryptoId, @RequestParam(required = false) String currency){
         return ResponseEntity.ok(marketService.getMarketPrices(cryptoId, currency));
     }
 
-    @GetMapping
-    public ResponseEntity<Map<String, Map<String, Double>>> getMultiplePrices(@RequestParam String assets, @RequestParam(required = false) String currency){
-        return ResponseEntity.ok(marketService.getMarketPrices(assets, currency));
+    @GetMapping("/prices")
+    public ResponseEntity<Map<String, Map<String, Double>>> getMultiplePrices(@RequestParam String cryptoIds, @RequestParam(required = false) String currency){
+        return ResponseEntity.ok(marketService.getMarketPrices(cryptoIds, currency));
+    }
+
+    @GetMapping("/top10")
+    public ResponseEntity<List<Top10CoinsDTO>> getTop10Coins(@RequestParam(required = false) String currency){
+        return ResponseEntity.ok(marketService.getTop10Coins(currency));
     }
 }
