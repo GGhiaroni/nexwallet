bd0bb9d54229c8666b6ca3a797e4f0a0cbfea810
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-10 15:25:15 -0300

    feat: Cria o novo método responsável por listar o top 10 moedas mais valiosas no momento em que a requisição é feita, fazendo a ponte entre o Controller e o Client

--- src/main/java/com/GabrielTiziano/NexWallet/service/MarketService.java
+++ src/main/java/com/GabrielTiziano/NexWallet/service/MarketService.java
@@ -1,9 +1,11 @@
 package com.GabrielTiziano.NexWallet.service;
 
 import com.GabrielTiziano.NexWallet.client.CoinGeckoClient;
+import com.GabrielTiziano.NexWallet.dto.Top10CoinsDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -18,5 +20,12 @@ public class MarketService {
                 : "usd";
 
         return coinGeckoClient.getPrice(cryptoIds, safeCurrency);
+    }
+
+    public List<Top10CoinsDTO> getTop10Coins(String currency){
+        String safeCurrency = (currency != null && !currency.isBlank())
+                ? currency.toLowerCase()
+                : "usd";
+        return coinGeckoClient.getTop10Coins(safeCurrency, "market_cap_desc", 10, 1);
     }
 }
