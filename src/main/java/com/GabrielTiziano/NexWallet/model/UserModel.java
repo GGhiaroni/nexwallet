9c871fa39ae0f876ded601520826d34040b99f20
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-10 18:20:07 -0300

    feat: Faz com que a UserModel implemente a classe UserDetails, para trabalhar com o Spring Security

--- src/main/java/com/GabrielTiziano/NexWallet/model/UserModel.java
+++ src/main/java/com/GabrielTiziano/NexWallet/model/UserModel.java
@@ -6,8 +6,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Data
@@ -15,7 +18,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "users")
-public class UserModel {
+public class UserModel implements UserDetails {
 
     @Id
     private String id;
@@ -29,4 +32,33 @@ public class UserModel {
     @Builder.Default
     private List<AssetModel> assets = new ArrayList<>();
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
