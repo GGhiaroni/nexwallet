1063c9633aa53944533e464831f8a456d9831540
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-10 11:16:02 -0300

    feat: Configura o client para salvar a resposta da requisição em cache e não estourar o rate limit da API do CoinGecko

--- src/main/java/com/GabrielTiziano/NexWallet/client/CoinGeckoClient.java
+++ src/main/java/com/GabrielTiziano/NexWallet/client/CoinGeckoClient.java
@@ -1,5 +1,6 @@
 package com.GabrielTiziano.NexWallet.client;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +9,7 @@ import java.util.Map;
 
 @FeignClient(name = "CoinGeckoClient", url = "${nexwallet.client.coingecko}")
 public interface CoinGeckoClient {
+    @Cacheable(value = "cotacoes", key = "#cryptoId + '-' + #currency")
     @GetMapping("/simple/price")
     Map<String, Map<String, Double>> getPrice(
             @RequestParam("ids") String cryptoId,
