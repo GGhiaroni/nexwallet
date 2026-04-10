2f9a88e45d6720fb98b1375223b9c101e35bbba1
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-10 10:24:52 -0300

    feat: Implementa o método responsável por listar o portfólio com todos os assets do usuário, que será chamado no Controller

--- src/main/java/com/GabrielTiziano/NexWallet/service/UserService.java
+++ src/main/java/com/GabrielTiziano/NexWallet/service/UserService.java
@@ -1,8 +1,7 @@
 package com.GabrielTiziano.NexWallet.service;
 
-import com.GabrielTiziano.NexWallet.dto.AssetDTO;
-import com.GabrielTiziano.NexWallet.dto.UserRequestDTO;
-import com.GabrielTiziano.NexWallet.dto.UserResponseDTO;
+import com.GabrielTiziano.NexWallet.client.CoinGeckoClient;
+import com.GabrielTiziano.NexWallet.dto.*;
 import com.GabrielTiziano.NexWallet.mapper.AssetMapper;
 import com.GabrielTiziano.NexWallet.mapper.UserMapper;
 import com.GabrielTiziano.NexWallet.model.AssetModel;
@@ -11,12 +10,17 @@ import com.GabrielTiziano.NexWallet.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
     private final UserRepository userRepository;
+    private final CoinGeckoClient coinGeckoClient;
 
     public UserResponseDTO registerUser(UserRequestDTO userRequestDTO){
          return UserMapper
@@ -34,5 +38,50 @@ public class UserService {
       UserModel userUpdated = userRepository.save(user);
 
       return UserMapper.toUserResponseDTO(userUpdated);
+    }
+
+    public PortfolioResponseDTO getUserPortfolio(String userId){
+        UserModel user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado na NexWallet!"));
+
+        if (user.getAssets().isEmpty()) {
+            return new PortfolioResponseDTO(0.0, new ArrayList<>());
+        }
+
+        String cryptoIds = user.getAssets().stream()
+                .map(AssetModel::getCryptoId)
+                .reduce((a, b) -> a + "," + b)
+                .orElse("");
+
+        Map<String, Map<String, Double>> apiResponse = coinGeckoClient.getPrice(cryptoIds, "usd");
+
+        Double totalBalance = 0.0;
+        List<AssetPortfolioDTO> portfolioAssets = new ArrayList<>();
+
+        for(AssetModel asset : user.getAssets()){
+
+            String coinName = asset.getCryptoId();
+
+            Double currentPrice = apiResponse.get(coinName).get("usd");
+
+            Double totalValue = asset.getQuantity() * currentPrice;
+
+            totalBalance += totalValue;
+
+            AssetPortfolioDTO assetPortfolioDTO = AssetPortfolioDTO
+                    .builder()
+                    .cryptoId(coinName)
+                    .quantity(asset.getQuantity())
+                    .currentPrice(currentPrice)
+                    .totalValue(totalValue)
+                    .build();
+
+            portfolioAssets.add(assetPortfolioDTO);
+        }
+
+        return PortfolioResponseDTO.builder()
+                .totalBalance(totalBalance)
+                .assets(portfolioAssets)
+                .build();
     }
 }
