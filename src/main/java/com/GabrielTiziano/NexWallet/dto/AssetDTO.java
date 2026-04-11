48e5f5331636365e4f76c80e4fa51db10d738a1e
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-11 10:16:14 -0300

    feat: Adiciona as annotations do Validation para estabelecer regras que o usuário deve seguir, como não permitir números negativos ou 0 de determinada moeda no AssetDTO, um formato de e-mail válido no LoginRequestDTO e UserRequestDTO, não passar valores null, etc

--- src/main/java/com/GabrielTiziano/NexWallet/dto/AssetDTO.java
+++ src/main/java/com/GabrielTiziano/NexWallet/dto/AssetDTO.java
@@ -1,5 +1,8 @@
 package com.GabrielTiziano.NexWallet.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +13,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AssetDTO {
+    @NotBlank(message = "O ID da criptomoeda é obrigatório (ex: bitcoin)")
     private String cryptoId;
+
+    @NotNull(message = "A quantidade é obrigatória")
+    @Positive(message = "A quantidade deve ser maior que zero")
     private Double quantity;
 }
--- src/main/java/com/GabrielTiziano/NexWallet/dto/LoginRequestDTO.java
+++ src/main/java/com/GabrielTiziano/NexWallet/dto/LoginRequestDTO.java
@@ -1,4 +1,15 @@
 package com.GabrielTiziano.NexWallet.dto;
 
-public record LoginRequestDTO(String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record LoginRequestDTO(
+        @NotBlank(message = "O e-mail não pode estar vazio")
+        @Email(message = "Formato de e-mail inválido")
+        String email,
+        @NotBlank(message = "A senha é obrigatória")
+        @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
+        String password
+) {
 }
